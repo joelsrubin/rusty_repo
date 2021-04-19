1419edda1889d99359b9ce794fff2a38116265ea
joelsrubin <joelsrubin@gmail.com>
2021-04-19 18:01:24 -0500

    error_handling etc

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,24 +1,41 @@
 
 use std::fs::File;
 use std::io::prelude::*;
-use std::error::Error;
+use std::error;
+use std::env;
 
 #[cfg(test)]
 mod test {
   use super::*;
 
   #[test]
-  fn one_result() {
+  fn case_sensitive() {
     let query = "duct";
     let contents = "\
     Rust:
     safe, fast, productive.
-    Pick three.";
+    Pick three.
+    Duct tape.";
 
     assert_eq!(
       vec!["safe, fast, productive."],
       search(query, contents)
     )
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+    Rust:
+    safe, fast, productive.
+    Pick three.
+    Trust me.";
+
+        assert_eq!(
+          vec!["Rust:", "Trust me."],
+          search_case_insensitive(query, contents)
+        );
   }
 }
 
@@ -32,12 +49,31 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   results
 }
 
-pub fn run(config: Config) -> Result<(), Box<Error>> {
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line.trim())
+    }
+  }
+  results
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
   let mut f = File::open(config.filename)?;
 
   let mut contents = String::new();
   f.read_to_string(&mut contents)?;
-  for line in search(&config.query, &contents) {
+
+  let results = if config.case_sensitive {
+    search(&config.query, &contents)
+  } else {
+    search_case_insensitive(&config.query, &contents)
+  };
+
+  for line in results {
     println!("{}", line)
   }
   Ok(())
@@ -46,6 +82,7 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 pub struct Config {
   pub query: String,
   pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
@@ -55,7 +92,8 @@ impl Config {
       }
       let query = args[1].clone();
       let filename = args[2].clone();
+      let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-      Ok(Config { query, filename })
+      Ok(Config { query, filename, case_sensitive })
   }
 }
